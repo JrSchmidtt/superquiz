29cc98e86b8fd1384b0114d6eb0f0a76f27b5667
Junior Schmidt <junior.schmidt.99@gmail.com>
2022-05-08 01:03:51 -0300

    update

--- index.js
+++ index.js
@@ -7,10 +7,20 @@ app.get("/:nome/:lang",(req,res)=>{
     var nome = req.params.nome;
     var lang = req.params.lang;
     var exibirMsg = true;
+
+    var produtos = [
+        {nome: "Doritos", preco: 15.65},
+        {nome: "Coca-cola", preco: 6.50},
+        {nome: "Nescau", preco: 1.50},
+        {nome: "Camiseta", preco: 85.00},
+        {nome: "Jaqueta", preco: 250.00}
+    ]
+
     res.render("index",{
         nome:nome,
         lang:lang,
-        msg: exibirMsg
+        msg: exibirMsg,
+        produtos: produtos
     })
 })
 
