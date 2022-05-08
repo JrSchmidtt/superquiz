adc1aeca09c404f9f994e2021eae21b283e93f2e
Junior Schmidt <junior.schmidt.99@gmail.com>
2022-05-08 02:35:54 -0300

    body parser

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
 
-app.set('view engine','ejs'); // difine o EJS como motor de vizualização em HTML
-app.use(express.static('public')); // Carrega arquivos estaticos
+// difine o EJS como motor de vizualização em HTML
+app.set('view engine','ejs'); 
+app.use(express.static('public'));
 
+// Body Paser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Rotas
 app.get("/",(req,res)=>{
     res.render("index",);
 })
@@ -11,5 +18,14 @@ app.get("/",(req,res)=>{
 app.get("/quiz",(req,res)=>{
     res.render("quiz",);
 })
+
+app.post("/sendform",(req,res)=>{
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulario recebido!");
+
+})
+
+
 
 app.listen(8181,()=>{console.log('App Rodando');});
