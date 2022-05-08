0aee5066000dbc294b8d057255882763ee830b1e
Junior Schmidt <junior.schmidt.99@gmail.com>
2022-05-08 01:50:48 -0300

    bootstrap front end

--- index.js
+++ index.js
@@ -4,25 +4,12 @@ const app = express();
 app.set('view engine','ejs'); // difine o EJS como motor de vizualização em HTML
 app.use(express.static('public')); // Carrega arquivos estaticos
 
-app.get("/:nome/:lang",(req,res)=>{
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = true;
+app.get("/",(req,res)=>{
+    res.render("index",);
+})
 
-    var produtos = [
-        {nome: "Doritos", preco: 15.65},
-        {nome: "Coca-cola", preco: 6.50},
-        {nome: "Nescau", preco: 1.50},
-        {nome: "Camiseta", preco: 85.00},
-        {nome: "Jaqueta", preco: 250.00}
-    ]
-
-    res.render("index",{
-        nome:nome,
-        lang:lang,
-        msg: exibirMsg,
-        produtos: produtos
-    })
+app.get("/quiz",(req,res)=>{
+    res.render("quiz",);
 })
 
 app.listen(8181,()=>{console.log('App Rodando');});
